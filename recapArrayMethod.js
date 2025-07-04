f620c144a2a85da1e85365dee03e519591becd49
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-04 20:36:27 +0600

    added all file update

--- recapArrayMethod.js
+++ recapArrayMethod.js
@@ -1,15 +1,113 @@
-const products = [
-    { id: 1, name: "iphone", color: "black", price: 12000, brand: "apple"},
+// const products = [
+//     { id: 1, name: "iphone", color: "black", price: 12000, brand: "apple"},
     
-    { id: 2, name: "xiaomi", color: "gold", price: 150000, brand: "xiaomi"},
-    { id: 3, name: "iphone", color: "black", price: 100, brand: "apple"},
-    { id: 4, name: "xiaomi", color: "gold", price: 1000, brand: "xiaomi"},
-    { id: 5, name: "samsumg", color: "black", price: 2000, brand: "samsumg"},
+//     { id: 2, name: "xiaomi", color: "gold", price: 150000, brand: "xiaomi"},
+//     { id: 3, name: "iphone", color: "black", price: 100, brand: "apple"},
+//     { id: 4, name: "xiaomi", color: "gold", price: 1000, brand: "xiaomi"},
+//     { id: 5, name: "samsumg", color: "black", price: 2000, brand: "samsumg"},
 
-];
+// ];
 
 
 
+
+
+
+
+// const mutatedApplePrices = products.map(p => {
+//     if (p.brand === "apple") {
+//         p.price += 100;
+//     }
+//     return p;
+// });
+// console.log(mutatedApplePrices);
+
+
+
+
+// const updatedApplePrices = products.map(p => {
+//     if (p.brand === "apple") {
+//         return { ...p, price: p.price + 100};
+//     }
+//     return p;
+// });
+// console.log(updatedApplePrices);
+
+// const appleProducts = products.filter(p => p.brand === "apple");
+// console.log(appleProducts);
+
+
+
+// products.forEach(p => {
+//     if (p.brand === "apple") {
+//         console.log(p);
+//     }
+// })
+
+
+
+// const goldProducts = products.find(p => p.color === "gold");
+// console.log(goldProducts);
+
+
+
+
+// const newProducts = products.filter(p => p.name === "iphone");
+// console.log(newProducts);
+
+
+
+
+// const iphoneProducts = products.filter(p => p.name === "iphone");
+// console.log(iphoneProducts);
+
+
+
+
+// const appleProducts = products.filter(p => p.brand === "apple");
+// console.log(appleProducts);
+
+
+
+// const newProducts = products.map(p => {
+//     if (p.brand === "apple") {
+//         return {...p, price: p.price + 100};
+//     }
+//     return p;
+// });
+// console.log(newProducts);
+
+
+
+
+// products.forEach(product => {
+//     if (product.brand === "apple") {
+//         console.log(product);
+//     }
+// });
+
+// const singleProduct = products.find(p => p.id === 3);
+// console.log(singleProduct);
+
+// const newProducts2 = products.filter(p => p.name === "iphone");
+// console.log(newProducts2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////
+
+// const newProducts2 = products.filter(p => p.name === "iphone");
+// console.log(newProducts2);
 
 
 // const singleProduct = products.find(p => p.color === "gold");
