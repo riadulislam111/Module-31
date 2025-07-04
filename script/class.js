d69c1e04737a18bd7d5347b0fe6b0338f2a205c0
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-04 21:13:19 +0600

    added all file update

--- script/class.js
+++ script/class.js
@@ -1,19 +1,43 @@
-class Product {
-    constructor(name, price,brand,malik) {
-        this.name = name;
-        this.price = price;
-        this.brandName = brand;
-        this.malik = malik;
+// class Product {
+//     constructor(name, price,brand,malik) {
+//         this.name = name;
+//         this.price = price;
+//         this.brandName = brand;
+//         this.malik = malik;
 
-    }
-    details() {
-        console.log(`ami ${this.name} product er details`)
-    }
-}
+//     }
+//     details() {
+//         console.log(`ami ${this.name} product er details`)
+//     }
+// }
 
-const iphone = new Product("iphone", "1234","apple", "jobs");
-iphone.details();
+// // const iphone = new Product("iphone", "1234","apple", "jobs");
+// // iphone.details();
 
-const xiaomi = new Product("redmi", 1000, "xiaomi", "jani na");
-console.log(xiaomi);
-xiaomi.details();
+// // const xiaomi = new Product("redmi", 1000, "xiaomi", "jani na");
+// // console.log(xiaomi);
+// // xiaomi.details();
+
+// const laptop = new Product("laptop", 45000, "Dell", "Michael");
+// laptop.details();
+// console.log(laptop);
+
+// const headphone = new Product("headphone", 1500, "Sony", "John" );
+// headphone.details();
+// console.log(headphone);
+
+
+
+// const tv = new Product("TV", 25000, "Samsung", "Rahim");
+// tv.details();
+// console.log(tv);
+
+// const smartwatch = new Product("Smart Watch", 5000, "Apple", "Sakib");
+// smartwatch.details();
+// console.log(smartwatch);
+
+// const tablet = new Product("Tablet", 20000, "Huawei", "Rafiq");
+// tablet.details();
+// console.log(tablet);
+
+
