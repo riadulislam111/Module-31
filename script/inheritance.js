567a1c8eff76f8d2dd188abe7b1058eb2ebd1455
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-05 13:05:02 +0600

    added all file update

--- script/inheritance.js
+++ script/inheritance.js
@@ -1,33 +1,141 @@
-class Vehical {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-    move() {
-        console.log("ami cholachol korte pari");
-    }
+// class Vehical {
+//     constructor(name, price) {
+//         this.name = name;
+//         this.price = price;
+//     }
+//     move() {
+//         console.log("ami cholachol korte pari");
+//     }
+// }
+
+// // // const { version } = require("react");
 
 
-}
+// // // // }
 
-class Bus extends Vehical {
-    constructor(name, price, seat) {
-        super(name, price);
-        this.seat = seat;
-    }
-    route() {
-        console.log("dhaka to cox's bazar")
-    }
-}
+// // // // // class Bus extends Vehical{
+// // // // //     constructor(name, price, seat) {
+// // // // //         super(name, price);
+// // // // //         this.seat = seat;
+// // // // //     }
+// // // // //     route() {
+// // // // //         console.log("dhaka to cox");
+// // // // //     }
+// // // // // }
 
-const greenLine = new Bus("Green Line", "12", 50);
-console.log(greenLine);
-greenLine.move();
+// // // // // const greenLine = new Bus("Green Line", "12", 50);
+// // // // // // greenLine.move();
+// // // // // console.log(greenLine);
 
-class Truck extends Vehical {
-    constructor(name, price, load) {
-        super(name, price);
-        this.load = load;
-    }
-};
+
+
+// // // // class Bus extends Vehical {
+// // // //     constructor(name, price, seat) {
+// // // //         super(name, price);
+// // // //         this.seat = seat;
+// // // //     }
+// // // //     route() {
+// // // //         console.log("dhaka to cox");
+// // // //     }
+// // // //     BusDetails() {
+// // // //         console.log(`Bus Name: ${this.name}, price: ${this.price}, seat Capacity: ${this.seat}`);
+
+// // // //     }
+// // // // }
+
+// // // // const greenLine = new Bus("Green Line", "12", 50);
+// // // // greenLine.BusDetails();
+
+
+// // // class Vehical  {
+// // //     constructor(name, price) {
+// // //         this.name = name;
+// // //         this.name = name;
+
+// // //     }
+// // //     move() {
+// // //         if (this.name === "Bus") {
+// // //             console.log("Bus is moving");
+// // //         }
+// // //         else if (this.name === "Car") {
+// // //             console.log("Car is moving");
+// // //         }
+// // //         else {
+// // //             console.log("Vehical is moving");
+// // //         }
+// // //     }
+// // // }
+
+// // // const myBus = new Vehical("Bus", 5000);
+// // // const myCar = new Vehical("Car", 3000);
+// // // myBus.move();
+// // // myCar.move();
+
+
+
+
+// // // class Bus extends Vehical {
+// // //     constructor(name, price, seat) {
+// // //         super(name, price);
+// // //         this.seat = seat;
+// // //     }
+// // //     route() {
+// // //         const route = ["Dhaka to Cox's Bazar"];
+// // //         const randomRoute = route[Math.floor(Math.random() * route.length)];
+// // //         console.log(randomRoute);
+// // //     }
+
+
+// // // }
+
+
+// // // const greenLine = new Bus("Green Line", "12", 50);
+// // // greenLine.route();
+// // // greenLine.move();
+
+// // class Bus extends Vehical {
+// //     constructor(name, price, seat) {
+// //         super(name, price);
+// //         this.seat = seat;
+// //     }
+// //     route() {
+// //         const routes = ["Dhaka to Cox's Bazar", "Dhaka to Chittagong", "Dhaka to Rajshahi"];
+// //         const randomRoute = routes[Math.floor(Math.random() * routes.length)];
+// //         console.log(randomRoute);
+// //     }
+// // }
+
+// // const greenLine = new Bus("Green Line", "12", 50);
+// // greenLine.route(); // Random route
+
+
+
+// class Bike extends Vehical {
+//     constructor(name, price, type) {
+//         super(name, price);
+//         this.type = type;
+//     }
+//     move() {
+//         console.log(`${this.name} is a ${this.type} bike and it's moving!`);
+//     }
+// }
+
+// class Truck extends Vehical {
+//     constructor(name, price, capacity) {
+//         super(name, price);
+//         this.capacity = capacity;
+//     }
+//     move() {
+//         console.log(`${this.name} truck is moving with ${this.capacity} capacity.`);
+//     }
+// }
+
+// const myBike = new Bike("Yamaha", 5000, "Sports");
+// const myTruck = new Truck("Volvo", 100000, "10 tons");
+
+// myBike.move(); // Output: Yamaha is a Sports bike and it's moving!
+// myTruck.move(); // Output: Volvo truck is moving with 10 tons capacity.
+
+
+
 
