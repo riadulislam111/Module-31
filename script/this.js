67833058c30978bf47ff507e77d286895dd6f224
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-07 15:00:33 +0600

    added all update file

--- script/this.js
+++ script/this.js
@@ -87,20 +87,70 @@
 
 
 
-class person {
-    #status;
-    constructor(name, age, status) {
-        this.name = name;
-        this.age = age;
-        this.#status = status;
-    }
-    action() {
-        console.log(this.#getStatus());
-    }
-    #getStatus() {
-        return this.#status;
-    }
-}
+// class person {
+//     #status;
+//     constructor(name, age, status) {
+//         this.name = name;
+//         this.age = age;
+//         this.#status = status;
+//     }
+//     action() {
+//         console.log(this.#getStatus());
+//     }
+//     #getStatus() {
+//         return this.#status;
+//     }
+// }
 
-const person3 = new person("Fatema", 27, "single");
-person3.action();
+// const person3 = new person("Fatema", 27, "single");
+// person3.action();
+
+
+
+// class person {
+//     #status;
+//     constructor(name, age, status) {
+//         this.name = name;
+//         this.age = age;
+//         this.#status = status;
+//     }
+//     sleep() {
+//         console.log(`${this.name} sleeps at 10PM`);
+//     }
+//     action() {
+//         this.sleep();
+//         console.log(this.#getStatus());
+//     }
+//     #getStatus() {
+//         return this.#status;
+//     }
+// }
+
+
+// const person4 = new person("Ruman", 22, "engaded");
+// person4.action();
+
+
+// class person {
+//     #status;
+//     constructor ( name, age, status) {
+//         this.name = name;
+//         this.age = age;
+//         this.#status = status;
+//     }
+//     action() {
+//         if (this.age > 18) {
+//             console.log(`${this.name} is an adult and is ${this.#getStatus()}`)
+//         } else {
+//             console.log(`${this.name} is a minor.`);
+//         }
+//     }
+//     #getStatus() {
+//         return this.#status;
+//     }
+// }
+
+// const person5 = new person("Raihan", 19, "single");
+// person5.action();
+
+
