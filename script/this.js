45ecf217bb587fb5fbffb07e5688f91d44b09e8f
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-07 14:36:43 +0600

    added all file update

--- script/this.js
+++ script/this.js
@@ -32,26 +32,55 @@
 // const solimuddin = new person("soiim", 12, "single");
 // solimuddin.action();
 
+// class person {
+//     #status;
+//     constructor (name, age, status) {
+//         this.name = name;
+//         this.age = age;
+//         this.#status = status;
+//     }
+//     sleep() {
+//         console.log(`${this.name} sleep at 10PM`);
+    
+//     }
+//     action() {
+//         console.log(this.#getStatus());
+//     }
+//     #getStatus(){
+//         return this.#status;
+//     }
+// }
+
+// const solimuddin = new person("solin", 12, "single");
+// solimuddin.action();
+
+
+
+
+// class person {
+//     constructor (name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// const person1 = new person("Riyad", 25);
+// console.log(person1.name);
+// console.log(person1.age);
+
+
+
 class person {
     #status;
-    constructor (name, age, status) {
+    constructor(name, age, status) {
         this.name = name;
         this.age = age;
         this.#status = status;
     }
-    sleep() {
-        console.log(`${this.name} sleep at 10PM`);
-    
-    }
     action() {
-        console.log(this.#getStatus());
-    }
-    #getStatus(){
-        return this.#status;
+        console.log(this.#status);
     }
 }
 
-const solimuddin = new person("solin", 12, "single");
-solimuddin.action();
-
-
+const person2 = new person("Ahmed", 30, "married");
+person2.action();
