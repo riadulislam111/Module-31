ee96d47e0743d6e326e1e90c9dd98f81dde63e27
riadulislam111 <.gitriadulislam.bd.pabna@gmail.com>
2025-07-09 12:15:28 +0600

    added all file update

--- script/summary.js
+++ script/summary.js
@@ -1,137 +1,183 @@
-const perosn = {
-    name: "gias",
-    age: 12,
-    1: 100
-}
-
-console.log(Object.keys(perosn));
-console.log(Object.values(perosn));
-// console.log(Object.entries(person));
-
-
-console.log(perosn.name)
-
-console.log(perosn["name"])
-console.log(perosn[1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const person = {
+// const perosn = {
 //     name: "gias",
 //     age: 12,
-//     1:100,
-//     father: {
-//         name:"dfdfd",
-//         age: 12,
-//     }
+//     1: 100
 // }
 
-// console.log(Object.keys(person));
-// console.log(Object.values(person))
-// console.log(Object.entries(person))
-// console.log(Object.entries(person))
-
-// console.log(person.name);
-
-// console.log(person["name"]);
-// console.log(person[1]);
-
-// console.log(person?.mother?.name);
-
-// const numbers = [23,4,3,45,564,545,5,3];
-
-// const newNumbers = numbers.map(num => num * num);
-// console.log(newNumbers);
+// // console.log(Object.keys(perosn));
+// // console.log(Object.values(perosn));
+// // // console.log(Object.entries(person));
 
 
+// // console.log(perosn.name)
+
+// // console.log(perosn["name"])
+// // console.log(perosn[1])
 
 
-// const products = [
-//     { name: "iphone", price: 1000 },
-//     { name: "samsung", price: 800 },
-//     { name: "google pixel", price: 900 }
-//   ];
+// // console.log(perosn?.mother?.name);
 
 
-//   products.forEach(p => {
-//     if(p.name === "iphone") {
-//       console.log(p);  
-//     }
-//   });
-
-
-// const products = [
-//     { id: 1, name: "iphone", color: "black", price: 12000, brand: "apple"},
-    
-//     { id: 2, name: "xiaomi", color: "black", price: 150000, brand: "xiaomi"},
-//     { id: 3, name: "iphone", color: "black", price: 100, brand: "apple"},
-//     { id: 4, name: "xiaomi", color: "black", price: 1000, brand: "xiaomi"},
-//     { id: 5, name: "samsumg", color: "black", price: 2000, brand: "samsumg"},
-
-// ];
-
-// const singleProduct = products.find(p => p.color === "black");
-// console.log(singleProduct);
+// // const numbers = [1,2,3,4,5,6,7];
+// // const newNumbers = numbers.map(num => {
+// //     return num * num;
+// // });
+// // console.log(newNumbers);
 
 
 
 
-// const newProduct = products.filter(p => p.id !=3);
-// console.log(newProduct);
+// // const products = [
+// //     {id: 1, name: "iphone", color: "black", price: 1090, brand: "apple"},
+// //     {id: 2, name: "xiaomi", color: "gold", price: 200, brand: "xiaomi"},
+// //     {id: 3, name: "samsung", color: "black", price: 120, brand: "samsung"},
+// //     {id: 4, name: "iphone", color: "gold", price: 1200, brand: "apple"},
+// //     {id: 5, name: "xiaomi", color: "black", price: 100, brand: "xiaomi"} // ঠিক করেছি এখানে
+// // ];
 
-
-// const newProduct = products.filter(p => p.color === "gold");
-// console.log(newProduct);
-
-
-// products.forEach(p => {
-//     if(p.name === "iphone"){
-//         console.log(p)
-//     }
-// })
-
-
-
-// class Person{
-//     constructor(name, age){
+// class Person (
+//     construtor(name, age) {
 //         this.age = age;
-//         this.name = name;
+//         this.name = name
 //     }
-// }
+// )
 
-// class Hena extends Person{
-//     constructor(name, age ,status){
-//         super(name,age)
-//     }
-// }
+// // class Hena extends Person{
+// //     constructor(name, age, status) {
+// //         super(name, age)
+// //     }
+// // }
 
-
-
-
-
+// const hena = new Person("hena", 21);
+// console.log(hena);
 
 
 
-// // const hena =new Person("hena", 21);
-// // console.log(hena);
+// // const singleProduct = products.find(p => p.color === "gold");
+// // console.log(singleProduct);
 
-// what will be the value of y? 
+// // const newProduct = products.filter(p => p.id !=3);
+// // console.log(newProduct);
 
-// // const {x, y, z} = {x: 1, y1: 2, z: 3};
-// // console.log(y)
-// const cube=x=> x*x*x; 
-// console.log(cube(2))
+// // const newProduct = products.filter(p => p.color === "gold");
+// // console.log(newProduct);
+
+// // products.forEach(p=> {
+// //     if (p.name === "iphone") {
+// //         console.log(p)
+// //     }
+// // });
+
+
+
+
+
+
+
+
+
+// // const person = {
+// //     name: "gias",
+// //     age: 12,
+// //     1:100,
+// //     father: {
+// //         name:"dfdfd",
+// //         age: 12,
+// //     }
+// // }
+
+// // console.log(Object.keys(person));
+// // console.log(Object.values(person))
+// // console.log(Object.entries(person))
+// // console.log(Object.entries(person))
+
+// // console.log(person.name);
+
+// // console.log(person["name"]);
+// // console.log(person[1]);
+
+// // console.log(person?.mother?.name);
+
+// // const numbers = [23,4,3,45,564,545,5,3];
+
+// // const newNumbers = numbers.map(num => num * num);
+// // console.log(newNumbers);
+
+
+
+
+// // const products = [
+// //     { name: "iphone", price: 1000 },
+// //     { name: "samsung", price: 800 },
+// //     { name: "google pixel", price: 900 }
+// //   ];
+
+
+// //   products.forEach(p => {
+// //     if(p.name === "iphone") {
+// //       console.log(p);  
+// //     }
+// //   });
+
+
+// // const products = [
+// //     { id: 1, name: "iphone", color: "black", price: 12000, brand: "apple"},
+    
+// //     { id: 2, name: "xiaomi", color: "black", price: 150000, brand: "xiaomi"},
+// //     { id: 3, name: "iphone", color: "black", price: 100, brand: "apple"},
+// //     { id: 4, name: "xiaomi", color: "black", price: 1000, brand: "xiaomi"},
+// //     { id: 5, name: "samsumg", color: "black", price: 2000, brand: "samsumg"},
+
+// // ];
+
+// // const singleProduct = products.find(p => p.color === "black");
+// // console.log(singleProduct);
+
+
+
+
+// // const newProduct = products.filter(p => p.id !=3);
+// // console.log(newProduct);
+
+
+// // const newProduct = products.filter(p => p.color === "gold");
+// // console.log(newProduct);
+
+
+// // products.forEach(p => {
+// //     if(p.name === "iphone"){
+// //         console.log(p)
+// //     }
+// // })
+
+
+
+// // class Person{
+// //     constructor(name, age){
+// //         this.age = age;
+// //         this.name = name;
+// //     }
+// // }
+
+// // class Hena extends Person{
+// //     constructor(name, age ,status){
+// //         super(name,age)
+// //     }
+// // }
+
+
+
+
+
+
+
+
+// // // const hena =new Person("hena", 21);
+// // // console.log(hena);
+
+// // what will be the value of y? 
+
+// // // const {x, y, z} = {x: 1, y1: 2, z: 3};
+// // // console.log(y)
+// // const cube=x=> x*x*x; 
+// // console.log(cube(2))
